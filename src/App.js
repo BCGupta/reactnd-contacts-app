ae86b0d2b237679bf1f583f6ffc308289216db73
Shubham Gupta <sguptadrums@gmail.com>
2020-05-06 14:24:24 -0700

    Adding project to Github

--- src/App.js
+++ src/App.js
@@ -6,7 +6,24 @@ import { Route } from 'react-router-dom'
 
 class App extends Component {
   state = {
-    contacts: []
+    contacts: [{
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+    },
+        {
+            "id": "richard",
+            "name": "Richard Kalehoff",
+            "handle": "richardkalehoff",
+            "avatarURL": "http://localhost:5001/richard.jpg"
+        },
+        {
+            "id": "tyler",
+            "name": "Tyler McGinnis",
+            "handle": "tylermcginnis",
+            "avatarURL": "http://localhost:5001/tyler.jpg"
+        }]
   }
   componentDidMount() {
     ContactsAPI.getAll()
