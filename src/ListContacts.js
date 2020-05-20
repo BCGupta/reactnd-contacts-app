0e66f493dc32b27a007fe263a3404f78bbc145e8
Shubham Gupta <sguptadrums@gmail.com>
2020-05-20 16:31:05 -0700

    Adding Controlled Components
    update Query and serach functionality

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-function ListContacts(props){
-    return(
+class ListContacts extends Component {
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequred,
+    }
 
+    state = {
+        query: ' '
+    }
+
+    updateQuery = (query) => {
+        this.setState(() => ({
+            query: query.trim()
+        }))
+    }
+
+render() {
+    const { query } = this.state
+    const { contacts, onDeleteContact } = this.props
+
+    const showingContacts = query === ''
+    ? contacts
+        : contacts.filter((c) => (
+            c.name.toLowerCase().includes(query.toLowerCase())
+        ))
+    return (
+<div className='list-contacts'>
+
+    <div className='list-contacts-top'>
+        <input
+            className='search-contacts'
+            type='text'
+            placeholder='Search Contacts'
+            value={query}
+            onChange={(event) => this.updateQuery(event.target.value)}
+            />
+    </div>
         <ol className={'contact-list'}>
-            {props.contacts.map((contact) =>
+            {showingContacts.map((contact) =>
                 <li key={contact.id} className={'contact-list-item'}>
                     <div
                         className={'contact-avatar'}
@@ -16,12 +51,19 @@ function ListContacts(props){
                     <div className={'contact-details'}>
                         <p>{contact.name}</p>
                         <p>{contact.handle}</p>
-                        <button className={'contact-remove'}> Remove </button>
+                        <button
+                            className={'contact-remove'}
+                            onClick={() => onDeleteContact(contact)}
+                        > Remove
+                        </button>
                     </div>
                 </li>
             )}
         </ol>
+</div>
     )
 }
+}
+
 
 export default ListContacts;
